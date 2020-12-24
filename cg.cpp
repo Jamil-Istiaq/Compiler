124ac022df32579c06a8de51024fa7d7bae5e63a
Md. Jamil Istiaq <60819346+jamil18-2@users.noreply.github.com>
2020-12-24 20:12:42 +0600

    Add files via upload

---
+++ cg.cpp
@@ -0,0 +1,50 @@
+// CPP program to print Happy Birthday
+#include<bits/stdc++.h>
+using namespace std;
+
+int i,j,k,l;
+char ha[20][20] = {'H','A','P','P','Y',' ','B','I','R','T','H','D','A','Y'}, you[100];
+int main()
+{
+
+cout<<"HH   HH       AAA       PPPPPP   PPPPPP    YY      YY"<<endl;
+cout<<"HH   HH      AA AA      PP   PP  PP   PP    YY    YY"<<endl;
+cout<<"HH   HH     AA   AA     PP    PP PP    PP    YY  YY"<<endl;
+cout<<"HH   HH    AA     AA    PP   PP  PP   PP       YYY"<<endl;
+cout<<"HHHHHHH   AAAAAAAAAAA   PPPPP    PPPPP         YY "<<endl;
+cout<<"HH   HH   AA       AA   PP       PP            YY"<<endl;
+cout<<"HH   HH   AA       AA   PP       PP            YY"<<endl;
+cout<<"HH   HH   AA       AA   PP       PP            YY\N"<<endl;
+
+
+cout<<"BBBBBB    IIIIIIIII  RRRRRRRR    TTTTTTTTTT    HH    HH"<<endl;
+cout<<"BB   BB      II      RR    RR        TT        HH    HH"<<endl;
+cout<<"BB   BB      II      RR     RR       TT        HH    HH"<<endl;
+cout<<"BB BB        II      RR    RR        TT        HH    HH"<<endl;
+cout<<"BBBB         II      RRR R           TT        HHHHHHHH"<<endl;
+cout<<"BB BB        II      RR   RR         TT        HH    HH"<<endl;
+cout<<"BB   BB      II      RR    RR        TT        HH    HH"<<endl;
+cout<<"BB   BB      II      RR     RR       TT        HH    HH"<<endl;
+cout<<"BBBBB     IIIIIIIII  RR      RR      TT        HH    HH\n"<<endl;
+
+
+cout<<"DDDD          AAA      YY        YY"<<endl;
+cout<<"DD  DD       AA AA      YY      YY"<<endl;
+cout<<"DD   DD     AA    AA     YY    YY"<<endl;
+cout<<"DD    DD   AA      AA     YY  YY"<<endl;
+cout<<"DD    DD  AA        AA     YYYY"<<endl;
+cout<<"DD   DD   AAAAAAAAAAAA      YY"<<endl;
+cout<<"DD  DD    AA        AA      YY"<<endl;
+cout<<"DD DD     AA        AA      YY"<<endl;
+cout<<"DDD       AA        AA      YY\n"<<endl;
+
+
+cout<<"  SSS        AAA         NN          NN     IIIIIIIII   YY          YY         AAA"<<endl;
+cout<<"SS   SS     AA  AA       NN NN       NN        II        YY        YY         AA  AA"<<endl;
+cout<<" SS   SS   AA    AA      NN  NN      NN        II          YY     YY         AA    AA"<<endl;
+cout<<"  SS      AA      AA     NN   NN     NN        II           YY   YY         AA      AA"<<endl;
+cout<<"   SS    AA        AA    NN    NN    NN        II            YY YY         AA        AA"<<endl;
+cout<<"    SS   AAAAAAAAAAAA    NN     NN   NN        II             YY           AAAAAAAAAAAA"<<endl;
+cout<<"SS   SS  AA        AA    NN       NN NN        II             YY           AA        AA"<<endl;
+cout<<"SS  SS   AA        AA    NN        NNNN        II             YY           AA        AA"<<endl;
+cout<<" SSS     AA        AA    NN          NN     IIIIIIIII         YY           AA        AA"<<endl;}
